eae9963330037c5d87205794c92296fa7d15f3ed
Knave <akahyulim@gmail.com>
2020-10-16 16:40:07 +0900

    Update StringUtils tests

--- Engine/Src/Core/StringUtils.cpp
+++ Engine/Src/Core/StringUtils.cpp
@@ -3,37 +3,37 @@
 
 namespace Dive
 {
-	std::string ToUppercase(const char * pLowerStr)
+	std::string ToUppercase(const char * lowerStr)
 	{
-		int size = static_cast<int>(strlen(pLowerStr));
+		int size = static_cast<int>(strlen(lowerStr));
 
 		std::string upperStr;
 		upperStr.reserve(size);
 
 		for (int i = 0; i != size; i++)
 		{
-			if (pLowerStr[i] >= 'a' && pLowerStr[i] <= 'z')
-				upperStr.push_back(pLowerStr[i] - ('a' - 'A'));
+			if (lowerStr[i] >= 'a' && lowerStr[i] <= 'z')
+				upperStr.push_back(lowerStr[i] - ('a' - 'A'));
 			else
-				upperStr.push_back(pLowerStr[i]);
+				upperStr.push_back(lowerStr[i]);
 		}
 
 		return upperStr;
 	}
 
-	std::string ToLowercase(const char * pUpperStr)
+	std::string ToLowercase(const char * upperStr)
 	{
-		int size = static_cast<int>(strlen(pUpperStr));
+		int size = static_cast<int>(strlen(upperStr));
 
 		std::string lowerStr;
 		lowerStr.reserve(size);
 
 		for (int i = 0; i != size; i++)
 		{
-			if (pUpperStr[i] >= 'A' && pUpperStr[i] <= 'Z')
-				lowerStr.push_back(pUpperStr[i] + ('a' - 'A'));
+			if (upperStr[i] >= 'A' && upperStr[i] <= 'Z')
+				lowerStr.push_back(upperStr[i] + ('a' - 'A'));
 			else
-				lowerStr.push_back(pUpperStr[i]);
+				lowerStr.push_back(upperStr[i]);
 		}
 
 		return lowerStr;
@@ -67,24 +67,24 @@ namespace Dive
 	// 주의: 최초 토큰에서 문자열 끝까지 리턴합니다.
 	// ex) "Our love is great" "love" => "love is great"
 	// ---------------------------------------------------------------------------------------------------------
-	std::string GetStringAfterToken(const char * pStr, const char * pToken)
+	std::string GetStringAfterToken(const char * str, const char * token)
 	{
-		int tokenSize = static_cast<int>(strlen(pToken));
+		int tokenSize = static_cast<int>(strlen(token));
 		char cmpStr[256];
 		std::string afterStr;
 
-		while (*pStr != '\0')
+		while (*str != '\0')
 		{
-			if (*pStr == *pToken)
+			if (*str == *token)
 			{
 				memset(cmpStr, 0, sizeof(cmpStr));
-				memcpy(cmpStr, pStr, tokenSize);
+				memcpy(cmpStr, str, tokenSize);
 
-				if (0 == strcmp(cmpStr, pToken))
-					afterStr = pStr;
+				if (0 == strcmp(cmpStr, token))
+					afterStr = str;
 			}
 
-			pStr++;
+			str++;
 		}
 
 		return afterStr;
@@ -92,12 +92,12 @@ namespace Dive
 
 	// ---------------------------------------------------------------------------------------------------------
 	// 주의: FirstToken, SecondToken 둘 다 처음 검색된 동일 토큰을 기준으로 작동합니다.
-	// ex) "Next Song: Plastic Love. Next Song: Plastic Love. " "Song: " " Next" => "Plastic Love."
+	// ex) "Next Song: Plastic Love." "Next" " Love" => " Song: Plastic ."
 	// ---------------------------------------------------------------------------------------------------------
-	std::string GetStringBetweenTokens(const char * pStr, const char * pFirstToken, const char * pSecondToken)
+	std::string GetStringBetweenTokens(const char * str, const char * firstToken, const char * secondToken)
 	{
-		int firstTokenSize = static_cast<int>(strlen(pFirstToken));
-		int secondTokenSize = static_cast<int>(strlen(pSecondToken));
+		int firstTokenSize = static_cast<int>(strlen(firstToken));
+		int secondTokenSize = static_cast<int>(strlen(secondToken));
 
 		char cmpStr[256];
 
@@ -105,32 +105,32 @@ namespace Dive
 
 		std::string betweenStr;
 
-		while (*pStr != '\0')
+		while (*str != '\0')
 		{
-			if (*pStr == *pFirstToken)
+			if (*str == *firstToken)
 			{
 				if (!isFoundFirstToken)
 				{
 					memset(cmpStr, 0, sizeof(cmpStr));
-					memcpy(cmpStr, pStr, firstTokenSize);
+					memcpy(cmpStr, str, firstTokenSize);
 
-					if (0 == strcmp(cmpStr, pFirstToken))
+					if (0 == strcmp(cmpStr, firstToken))
 					{
 						isFoundFirstToken = true;
-						pStr += firstTokenSize;
+						str += firstTokenSize;
 					}
 				}
 			}
 
 			// 첫 번째 토큰 후 바로 위치할 수 있다.
-			if (*pStr == *pSecondToken)
+			if (*str == *secondToken)
 			{
 				if (isFoundFirstToken)
 				{
 					memset(cmpStr, 0, sizeof(cmpStr));
-					memcpy(cmpStr, pStr, secondTokenSize);
+					memcpy(cmpStr, str, secondTokenSize);
 
-					if (0 == strcmp(cmpStr, pSecondToken))
+					if (0 == strcmp(cmpStr, secondToken))
 					{
 						if (betweenStr.empty())
 						{
@@ -143,39 +143,39 @@ namespace Dive
 			}
 
 			if (isFoundFirstToken)
-				betweenStr += *pStr;
+				betweenStr += *str;
 
-			pStr++;
+			str++;
 		}
 
 		return betweenStr;
 	}
 
-	std::string ReplaceToken(const char * pStr, const char * pToken, const char * pNewToken)
+	std::string ReplaceToken(const char * str, const char * token, const char * newToken)
 	{
-		int tokenSize = static_cast<int>(strlen(pToken));
+		int tokenSize = static_cast<int>(strlen(token));
 		char cmpStr[256];
 		std::string fixedStr;
 
-		while (*pStr != '\0')
+		while (*str != '\0')
 		{
-			if (*pStr == *pToken)
+			if (*str == *token)
 			{
 				memset(cmpStr, 0, sizeof(cmpStr));
-				memcpy(cmpStr, pStr, tokenSize);
+				memcpy(cmpStr, str, tokenSize);
 
-				if (0 == strcmp(cmpStr, pToken))
+				if (0 == strcmp(cmpStr, token))
 				{
-					fixedStr += pNewToken;
-					pStr += tokenSize - 1;
+					fixedStr += newToken;
+					str += tokenSize - 1;
 				}
 				else
-					fixedStr += *pStr;
+					fixedStr += *str;
 			}
 			else
-				fixedStr += *pStr;
+				fixedStr += *str;
 
-			pStr++;
+			str++;
 		}
 
 		return fixedStr;
--- Tests/Src/Test_StringUtils.cpp
+++ Tests/Src/Test_StringUtils.cpp
@@ -22,34 +22,12 @@ TEST(StringUtils, GetStringAfterToken)
 	ASSERT_STREQ("love is great.", Dive::GetStringAfterToken("Our love is great.", "love").c_str());
 }
 
-//std::string GetStringBetweenTokens(const char* pStr, const char* pFirstToken, const char* pSecondToken);
 TEST(StringUtils, GetStringBetweenTokens)
 {
+	ASSERT_STREQ(" Song: Plastic ", Dive::GetStringBetweenTokens("Next Song: Plastic Love.", "Next", "Love").c_str());
 }
 
-//std::string ReplaceToken(const char* pStr, const char* pToken, const char* pNewToken);
 TEST(StringUtils, ReplaceToken)
 {
-}
-
-// UNICODE
-//std::wstring MbcsToWbcs(const std::string str, unsigned int mbcsEncoding = CP_ACP);
-TEST(StringUtils, MbcsToWbcs)
-{
-}
-//std::string WbcsToMbcs(const std::wstring str, unsigned int mbcsEncoding = CP_ACP);
-TEST(StringUtils, WbcsToMbcs)
-{
-}
-//std::string ConvertMbcs(const std::string str, unsigned int fromEncoding, unsigned int toEncoding);
-TEST(StringUtils, ConvertMbcs)
-{
-}
-//std::wstring Utf8ToUtf16(const std::string str);
-TEST(StringUtils, Utf8ToUtf16)
-{
-}
-//std::string Utf16ToUtf8(const std::wstring str);
-TEST(StringUtils, Utf16ToUtf8)
-{
+	ASSERT_STREQ("Adobe Imageshop CC", Dive::ReplaceToken("Adobe Photoshop CC", "Photo", "Image").c_str());
 }
