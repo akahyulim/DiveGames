632ff321c665fde2769861dd426b9c6eee656f06
Knave <akahyulim@gmail.com>
2020-10-17 16:01:29 +0900

    Implement D3D11 RenderDevice's ConstantBuffer

---
+++ Engine/Src/Rendering/D3D11/ConstantBuffer.cpp
@@ -0,0 +1,52 @@
+#include "DivePch.h"
+#include "ConstantBuffer.h"
+#include "RenderDevice.h"
+
+namespace Dive
+{
+	ConstantBuffer::ConstantBuffer(RenderDevice* device)
+	{
+		if (!device || !device->GetD3DDevice() || !device->GetImmediateContext())
+		{
+			CORE_ERROR("ConstantBuffer::ConstantBuffer>> RenderDevice 객체가 유효하지 않습니다.");
+			return;
+		}
+
+		m_device = device;
+	}
+
+	ConstantBuffer::~ConstantBuffer()
+	{
+	}
+
+	void * ConstantBuffer::Map()
+	{
+		if (!m_buffer)
+		{
+			CORE_ERROR("ConstantBuffer::Map>> Buffer가 유효하지 않습니다.");
+			return nullptr;
+		}
+
+		D3D11_MAPPED_SUBRESOURCE mappedResource;
+		if (FAILED(m_device->GetImmediateContext()->Map(static_cast<ID3D11Resource*>(m_buffer.get()), 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource)))
+		{
+			CORE_ERROR("ConstantBuffer::Map>> Constant Buffer Map에 실패하였습니다.");
+			return nullptr;
+		}
+
+		return mappedResource.pData;
+	}
+
+	bool ConstantBuffer::Unmap()
+	{
+		if (!m_buffer)
+		{
+			CORE_ERROR("ConstantBuffer::Unmap>> Buffer가 유효하지 않습니다.");
+			return false;
+		}
+
+		m_device->GetImmediateContext()->Unmap(static_cast<ID3D11Resource*>(m_buffer.get()), 0);
+
+		return true;
+	}
+}
---
+++ Engine/Src/Rendering/D3D11/ConstantBuffer.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "Core/Object.h"
+#include "Core/Log.h"
+
+namespace Dive
+{
+	class RenderDevice;
+
+	class ConstantBuffer : public Object
+	{
+	public:
+		ConstantBuffer(RenderDevice* device);
+		~ConstantBuffer();
+
+		template<typename T>
+		bool Create()
+		{
+			D3D11_BUFFER_DESC descBuffer;
+			ZeroMemory(&descBuffer, sizeof(descBuffer));
+			descBuffer.BindFlags			= D3D11_BIND_CONSTANT_BUFFER;
+			descBuffer.ByteWidth			= static_cast<UINT>(sizeof(T));
+			descBuffer.CPUAccessFlags		= D3D11_CPU_ACCESS_WRITE;
+			descBuffer.MiscFlags			= 0;
+			descBuffer.StructureByteStride	= 0;
+			descBuffer.Usage				= D3D11_USAGE_DYNAMIC;
+
+			auto buffer_ptr = m_buffer.get();
+			if (FAILED(m_device->GetD3DDevice()->CreateBuffer(&descBuffer, nullptr, &buffer_ptr)))
+			{
+				CORE_ERROR("ConstantBuffer::Create >> Buffer 생성에 실패하였습니다.");
+				return false;
+			}
+
+			return true;
+		}
+
+		void* Map();
+		bool Unmap();
+
+		ID3D11Buffer* GetBuffer() { return m_buffer.get(); }
+
+	private:
+		RenderDevice* m_device;
+		std::shared_ptr<ID3D11Buffer> m_buffer;
+	};
+}
+
