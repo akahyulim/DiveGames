e0f04c3282b2ca6e6c7dd736043ea1174e8a4393
Knave <akahyulim@gmail.com>
2021-07-21 17:44:11 +0900

    Transform의 나머지 기능 구현 중

--- DiveEngine/Transform.cpp
+++ DiveEngine/Transform.cpp
@@ -11,7 +11,7 @@ namespace dive
 		: Component(typeid(Transform).hash_code(), pGameObject, this)
 	{
 		m_LocalPosition = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f);
-		m_LocalRotation = DirectX::XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
+		DirectX::XMStoreFloat4(&m_LocalRotation, DirectX::XMQuaternionIdentity());
 		m_LocalScale = DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f);
 
 		DirectX::XMStoreFloat4x4(&m_Matrix, DirectX::XMMatrixIdentity());
@@ -35,7 +35,9 @@ namespace dive
 
 	void Transform::Update(float deltaTime)
 	{
-		
+		//test
+
+		translate(DirectX::XMVectorSet(0.0f, 0.1f, 0.0f, 1.0f) * deltaTime, eSpace::World);
 	}
 
 	void Transform::Serialize(FileStream* pFileStream)
@@ -62,9 +64,9 @@ namespace dive
 	// 변환 행렬은 구현을 완료하였다.
 	void Transform::UpdateTransform()
 	{
-		DirectX::XMMATRIX matLocalPos = DirectX::XMMatrixTranslationFromVector(getLocalPositionByVector());
+		DirectX::XMMATRIX matLocalPos = DirectX::XMMatrixTranslationFromVector(getLocalPosition());
 		DirectX::XMMATRIX matLocalRot = DirectX::XMMatrixIdentity();//DirectX::XMMatrixRotationQuaternion(GetLocalRotationVector());
-		DirectX::XMMATRIX matLocalScl = DirectX::XMMatrixScalingFromVector(getLocalScaleByVector());
+		DirectX::XMMATRIX matLocalScl = DirectX::XMMatrixScalingFromVector(getLocalScale());
 
 		auto localMatrix = matLocalScl * matLocalRot * matLocalPos;
 		DirectX::XMStoreFloat4x4(&m_LocalMatrix, localMatrix);
@@ -91,19 +93,37 @@ namespace dive
 
 	void Transform::SetPosition(const DirectX::XMFLOAT3& position)
 	{
-		setPositionByVector(DirectX::XMLoadFloat3(&position));
+		setPosition(DirectX::XMLoadFloat3(&position));
 	}
 
 	void Transform::SetLocalPosition(const DirectX::XMFLOAT3& position)
 	{
-		setLocalPositionByVector(DirectX::XMLoadFloat3(&position));
+		setLocalPosition(DirectX::XMLoadFloat3(&position));
+	}
+
+	DirectX::XMFLOAT4 Transform::GetRotation() const
+	{
+		return DirectX::XMFLOAT4();
+	}
+
+	void Transform::SetRotation(const DirectX::XMFLOAT4& rotation)
+	{
+	}
+
+	void Transform::SetLocalRotation(const DirectX::XMFLOAT4& rotation)
+	{
+	}
+
+	void Transform::SetRotation(float xAngle, float yAngle, float zAngle)
+	{
+		// 일단 라디안으로 바꾼 후 어떻게 한다.
 	}
 	
 	DirectX::XMFLOAT3 Transform::GetScale() const
 	{
 		if (HasParent())
 		{
-			auto scale = DirectX::XMVector3Transform(getLocalScaleByVector(), m_pParent->GetMatrix());
+			auto scale = DirectX::XMVector3Transform(getLocalScale(), m_pParent->GetMatrix());
 			DirectX::XMFLOAT3 result;
 			DirectX::XMStoreFloat3(&result, scale);
 
@@ -115,10 +135,10 @@ namespace dive
 	
 	void Transform::SetScale(const DirectX::XMFLOAT3& scale)
 	{
-		setScaleByVector(DirectX::XMLoadFloat3(&scale));
+		setScale(DirectX::XMLoadFloat3(&scale));
 	}
 
-	// 이건 다시 setLocalScaleByVector로 바꾸자
+	// 이건 다시 setLocalScale로 바꾸자
 	void Transform::SetLocalScale(const DirectX::XMFLOAT3& scale)
 	{
 		// 흐음... 0.0이어도 안되는데
@@ -131,35 +151,54 @@ namespace dive
 		UpdateTransform();
 	}
 
+	// 특성상 deltaTime을 곱하는 경우가 많은데 XMFLOAT3는 그게 안된다.
+	void Transform::Translate(const DirectX::XMFLOAT3& translation, eSpace relativeTo)
+	{
+		translate(DirectX::XMLoadFloat3(&translation), relativeTo);
+	}
+
+	void Transform::Translate(float x, float y, float z, eSpace relativeTo)
+	{
+		translate(DirectX::XMVectorSet(x, y, z, 1.0f), relativeTo);
+	}
+
+	void Transform::Rotate(const DirectX::XMFLOAT3& enulerAngles)
+	{
+	}
+
+	void Transform::Rotate(float xAngle, float yAngle, float zAngle)
+	{
+	}
+
 	void Transform::SetLookAt(float x, float y, float z)
 	{
 		m_LookAt = DirectX::XMFLOAT3(x, y, z);
 	}
 
-	DirectX::XMVECTOR Transform::getPositionByVector() const
+	DirectX::XMVECTOR Transform::getPosition() const
 	{
 		return DirectX::XMVectorSet(m_Matrix._41, m_Matrix._42, m_Matrix._43, 1.0f);
 	}
 
-	void Transform::setPositionByVector(const DirectX::FXMVECTOR& position)
+	void Transform::setPosition(const DirectX::FXMVECTOR& position)
 	{
-		if (DirectX::XMVector3Equal(position, getPositionByVector()))
+		if (DirectX::XMVector3Equal(position, getPosition()))
 			return;
 
-		setLocalPositionByVector(HasParent() ?
+		setLocalPosition(HasParent() ?
 			DirectX::XMVector3Transform(position, DirectX::XMMatrixInverse(nullptr, m_pParent->GetMatrix()))
 			: position);
 	}
 
-	DirectX::XMVECTOR Transform::getLocalPositionByVector() const
+	DirectX::XMVECTOR Transform::getLocalPosition() const
 	{
 		return DirectX::XMLoadFloat3(&m_LocalPosition);
 	}
 
-	// scale처럼 바꿀건지 선택해야 한다.
-	void Transform::setLocalPositionByVector(const DirectX::FXMVECTOR& position)
+	// 이게 낫다.
+	void Transform::setLocalPosition(const DirectX::FXMVECTOR& position)
 	{
-		if (DirectX::XMVector3Equal(position, getLocalPositionByVector()))
+		if (DirectX::XMVector3Equal(position, getLocalPosition()))
 			return;
 
 		DirectX::XMStoreFloat3(&m_LocalPosition, position);
@@ -167,19 +206,69 @@ namespace dive
 		UpdateTransform();
 	}
 
+	DirectX::XMVECTOR Transform::getRotation() const
+	{
+		// 이게 맞나...
+		XMFLOAT3 scale;
+		XMStoreFloat3(&scale, getScale());
+
+		if (scale.x == 0.0f || scale.y == 0.0f || scale.z == 0.0f)
+			return XMVectorSet(0.0f, 0.0f, 0.0f, 1.0f);
+
+		XMFLOAT4X4 mat;
+		XMStoreFloat4x4(&mat, GetMatrix());
+
+		// 결국 변환행렬에서 각 요소의 scale을 나눈 후 결과를 다시 쿼터니언으로 변환하는 거다.
+		// 문제는 변환행렬에서 scale을 계산하는 방법이 이해가지 않는다는 거다.
+		XMFLOAT4X4 rot;
+		rot._11 = mat._11 / scale.x;	rot._12 = mat._12 / scale.x;	rot._13 = mat._13 / scale.x;	rot._14 = 0.0f;
+		rot._21 = mat._21 / scale.y;	rot._22 = mat._22 / scale.y;	rot._23 = mat._23 / scale.y;	rot._24 = 0.0f;
+		rot._31 = mat._31 / scale.z;	rot._32 = mat._32 / scale.z;	rot._33 = mat._33 / scale.z;	rot._34 = 0.0f;
+		rot._41 = 0.0f;					rot._42 = 0.0f;					rot._43 = 0.0f;					rot._44 = 1.0f;
+
+		return XMQuaternionRotationMatrix(XMLoadFloat4x4(&rot));
+	}
+
+	void Transform::setRotation(const DirectX::FXMVECTOR& rotation)
+	{
+		if (DirectX::XMQuaternionEqual(rotation, getRotation()))
+			return;
+
+		// 부모의 월드 회전 쿼터니언의 역수를 곱한다.
+		// 왜인지 까먹었다.
+		setLocalPosition(HasParent() ?
+			DirectX::XMQuaternionMultiply(rotation, DirectX::XMQuaternionInverse(GetParent()->getRotation()))
+			: rotation);
+	}
+
+	DirectX::XMVECTOR Transform::getLocalRotation() const
+	{
+		return DirectX::XMLoadFloat4(&m_LocalRotation);
+	}
+
+	void Transform::setLocalRotation(const DirectX::FXMVECTOR& rotation)
+	{
+		if (DirectX::XMQuaternionEqual(rotation, getLocalRotation()))
+			return;
+
+		DirectX::XMStoreFloat4(&m_LocalRotation, rotation);
+
+		UpdateTransform();
+	}
+
 	// 일단 World Scale 계산은 틀린 것 같다.
-	DirectX::XMVECTOR Transform::getScaleByVector() const
+	DirectX::XMVECTOR Transform::getScale() const
 	{
 		if (HasParent())
 		{
-			auto scale = DirectX::XMVector3Transform(getLocalScaleByVector(), m_pParent->GetMatrix());
+			auto scale = DirectX::XMVector3Transform(getLocalScale(), m_pParent->GetMatrix());
 			return scale;
 		}
 
 		return DirectX::XMLoadFloat3(&m_LocalScale);
 	}
 
-	void Transform::setScaleByVector(const DirectX::FXMVECTOR& scale)
+	void Transform::setScale(const DirectX::FXMVECTOR& scale)
 	{
 		// 비교는 나중에
 
@@ -187,26 +276,46 @@ namespace dive
 		{
 			// 부모가 있을 때
 			// 일단 여기에서 역 곱하기
-			setLocalScaleByVector(DirectX::XMVector3Transform(scale, DirectX::XMMatrixInverse(nullptr, m_pParent->GetMatrix())));
+			setLocalScale(DirectX::XMVector3Transform(scale, DirectX::XMMatrixInverse(nullptr, m_pParent->GetMatrix())));
 		}
 		else
 		{
 			// 부모가 없을 때
-			setLocalScaleByVector(scale);
+			setLocalScale(scale);
 		}
 	}
 
 	// 로컬은 그냥 가져와도 되나?
-	DirectX::XMVECTOR Transform::getLocalScaleByVector() const
+	DirectX::XMVECTOR Transform::getLocalScale() const
 	{
 		return DirectX::XMLoadFloat3(&m_LocalScale);
 	}
 
-	void Transform::setLocalScaleByVector(const DirectX::FXMVECTOR& scale)
+	// 이건 다시 바꾸자.
+	void Transform::setLocalScale(const DirectX::FXMVECTOR& scale)
 	{
 		DirectX::XMFLOAT3 destination;
 		DirectX::XMStoreFloat3(&destination, scale);
 
 		SetLocalScale(destination);
+	}
+
+	void Transform::translate(const DirectX::FXMVECTOR& translation, eSpace relativeTo)
+	{
+		switch (relativeTo)
+		{
+		case eSpace::World:
+		{
+			setPosition(getPosition() + translation);
+			return;
+		}
+		case eSpace::Self:
+		{
+			// 회전행렬을 곱해야 한다.
+			return;
+		}
+		default:
+			return;
+		}
 	}
 }
--- DiveEngine/Transform.h
+++ DiveEngine/Transform.h
@@ -9,6 +9,12 @@ namespace dive
 	class GameObject;
 	class FileStream;
 
+	enum class eSpace
+	{
+		World,
+		Self,
+	};
+
 	class Transform : public Component
 	{
 	public:
@@ -30,12 +36,13 @@ namespace dive
 		void SetLocalPosition(const DirectX::XMFLOAT3& position);
 		
 		// Rotation
-		// Position, Scale과 달라야 한다.
-		// Quaternion, eulerAngles, Radian으로 다뤄지기 때문이다.
-		// 너무 많다. 유니티는 뭘루 다뤘을까?
-		// 유니티는 일단 Quaternion으로 world, local rotation을 가진다.
-		// 그리고 Rotate 함수에서는 Vector3로 eulerAngles를 받는다.
-
+		DirectX::XMFLOAT4 GetRotation() const;
+		void SetRotation(const DirectX::XMFLOAT4& rotation);
+		DirectX::XMFLOAT4 GetLocalRotation() const { return m_LocalRotation; }
+		void SetLocalRotation(const DirectX::XMFLOAT4& rotation);
+		// eular angle을 받는 것들도 필요하....나?
+		// 유니티의 경우 Quaternion에 Eular 함수가 있어서 변환시켜 준다...
+		void SetRotation(float xAngle, float yAngle, float zAngle);
 
 		// Scale
 		DirectX::XMFLOAT3 GetScale() const;
@@ -43,8 +50,11 @@ namespace dive
 		DirectX::XMFLOAT3 GetLocalScale() const { return m_LocalScale; }
 		void SetLocalScale(const DirectX::XMFLOAT3& scale);
 
-		// translate
-		// rotate
+		void Translate(const DirectX::XMFLOAT3& translation, eSpace relativeTo = eSpace::Self);
+		void Translate(float x, float y, float z, eSpace relativeTo = eSpace::Self);
+
+		void Rotate(const DirectX::XMFLOAT3& enulerAngles);
+		void Rotate(float xAngle, float yAngle, float zAngle);
 
 		void SetLookAtByFloat3(const DirectX::XMFLOAT3& lookAt) { m_LookAt = lookAt; }
 		void SetLookAt(float x, float y, float z);
@@ -70,15 +80,24 @@ namespace dive
 
 
 	private:
-		DirectX::XMVECTOR getPositionByVector() const;
-		void setPositionByVector(const DirectX::FXMVECTOR& position);
-		DirectX::XMVECTOR getLocalPositionByVector() const;
-		void setLocalPositionByVector(const DirectX::FXMVECTOR& position);
+		// 흐음... 얘네를 다시 public으로 올려야 할 거 같다.
+		// deltaTime을 곱할때는 VECTOR가 더 편하기 때문이다.
+		DirectX::XMVECTOR getPosition() const;
+		void setPosition(const DirectX::FXMVECTOR& position);
+		DirectX::XMVECTOR getLocalPosition() const;
+		void setLocalPosition(const DirectX::FXMVECTOR& position);
 
-		DirectX::XMVECTOR getScaleByVector() const;
-		void setScaleByVector(const DirectX::FXMVECTOR& scale);
-		DirectX::XMVECTOR getLocalScaleByVector() const;
-		void setLocalScaleByVector(const DirectX::FXMVECTOR& scale);
+		DirectX::XMVECTOR getRotation() const;
+		void setRotation(const DirectX::FXMVECTOR& rotation);
+		DirectX::XMVECTOR getLocalRotation() const;
+		void setLocalRotation(const DirectX::FXMVECTOR& rotation);
+
+		DirectX::XMVECTOR getScale() const;
+		void setScale(const DirectX::FXMVECTOR& scale);
+		DirectX::XMVECTOR getLocalScale() const;
+		void setLocalScale(const DirectX::FXMVECTOR& scale);
+
+		void translate(const DirectX::FXMVECTOR& translation, eSpace relativeTo);
 		
 	private:
 		bool m_bChanged = false;
