74338ded0aa85a3736939f66fd3fe81232db74e4
Knave <akahyulim@gmail.com>
2022-09-08 17:01:05 +0900

    test : 기존 테스트 삭제

--- Sandbox/Src/Sandbox.cpp
+++ Sandbox/Src/Sandbox.cpp
@@ -39,63 +39,7 @@ namespace Sandbox
 		
 		// test
 		{
-			// vertices, indices 积己
-			std::vector<Dive::VertexElement> elements;
-			elements.emplace_back(Dive::eVertexElementType::TYPE_VECTOR3, Dive::eVertexElementSemantic::SEM_POSITION);
-			//elements.emplace_back(Dive::eVertexElementType::TYPE_VECTOR3, Dive::eVertexElementSemantic::SEM_NORMAL);
-			//elements.emplace_back(Dive::eVertexElementType::TYPE_VECTOR2, Dive::eVertexElementSemantic::SEM_TEXCOORD);
-			//elements.emplace_back(Dive::eVertexElementType::TYPE_VECTOR2, Dive::eVertexElementSemantic::SEM_TEXCOORD, 1);
-
-			Dive::VertexBuffer vb(GetContext());
-			vb.CreateBuffer(3, elements);
-
-			std::vector<DirectX::XMFLOAT3> triangle;
-			triangle.emplace_back(-0.5f, -0.5f, 0.0f);
-			triangle.emplace_back(0.5f, -0.5f, 0.0f);
-			triangle.emplace_back(0.5f, 0.5f, 0.0f);
-
-			vb.SetData(triangle.data());
-
-			Dive::IndexBuffer ib(GetContext());
-			ib.CreateBuffer(3, false);
-
-			std::vector<unsigned int> indices;
-			indices.emplace_back(0);
-			indices.emplace_back(1);
-			indices.emplace_back(2);
-			ib.SetData(indices.data());
 			
-
-			// shader 积己
-			Dive::Shader vs(m_pContext, Dive::eShaderType::Vertex);
-			vs.Compile("../CoreData/Shaders/test.hlsl");
-			Dive::Shader ps(m_pContext, Dive::eShaderType::Pixel);
-			ps.Compile("../CoreData/Shaders/test.hlsl");
-
-			// input layout 积己
-			Dive::InputLayout il(m_pContext, &vb, &vs);
-
-			// buffer bind
-			auto pDeviceContext = GetSubsystem<Dive::Graphics>()->GetDeviceContext();
-			
-			auto* pVB = vb.GetBuffer();
-			UINT stride = vb.GetVertexStride();
-			UINT offset = 0;
-			pDeviceContext->IASetVertexBuffers(0, 1, &pVB, &stride, &offset);
-			pDeviceContext->IASetIndexBuffer(ib.GetBuffer(), ib.GetFormat(), 0);
-
-			// shader bind
-			pDeviceContext->VSSetShader(reinterpret_cast<ID3D11VertexShader*>(vs.GetShader()), NULL, 0);
-			pDeviceContext->PSSetShader(reinterpret_cast<ID3D11PixelShader*>(ps.GetShader()), NULL, 0);
-
-			// inputlayout bind
-			pDeviceContext->IASetInputLayout(il.GetInputLayout());
-
-			// destroy
-			vb.Release();
-			ib.Release();
-			vs.Release();
-			ps.Release();
 		}
 
 		// camera绰 糕滚 函荐肺 喊档 包府
